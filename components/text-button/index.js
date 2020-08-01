e6e5c991b13a275b11c21897da4e28517c5c1523
Nicolas Rotier <nicolas@beekast.com>
2020-08-01 12:37:47 +0200

    start of react CV

--- components/text-button/index.js
+++ components/text-button/index.js
@@ -1,6 +1,6 @@
 import styles from './button.module.scss'
 
-export default function Button({ text, children }) {
+export default function TextButton({ text, children }) {
   return (
     <div className={styles.button} onClick={() => alert('test')}>
       {children}
